28f7d118be7367de175302edb2b7e72e860e649c
Muhammad Hashim Ali <hashimtrox@gmail.com>
2025-05-18 13:16:18 +0500

    Micro Services

--- backend/api_gateway/server.js
+++ backend/api_gateway/server.js
@@ -10,20 +10,31 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
-app.use('/auth', createProxyMiddleware({target: 'http://auth-service:3001',
+app.use('/auth', createProxyMiddleware({
+    target: 'http://localhost:5001',
     changeOrigin: true,
+    pathRewrite: {
+        "^": "auth",
+    },
     secure: false,
 }));
 
-app.use('/users', createProxyMiddleware({target: 'http://user-service:3002',
+app.use('/users', createProxyMiddleware({
+    target: 'http://user-service:5002',
     changeOrigin: true,
+    pathRewrite: {
+        "^": "users",
+    },
     secure: false,
 }))
 
-app.use('/excercises', createProxyMiddleware({target: 'http://exercise-service:3003',
+app.use('/excercises', createProxyMiddleware({
+    target: 'http://exercise-service:5003',
     changeOrigin: true,
+    pathRewrite: {
+        "^": "excercises",
+    },
     secure: false
 }))
 
